9a9d02f366bd5b02b757469001ad72857115c841
Lonsper <lonsper@gmail.com>
2018-10-20 15:48:50 +0200

    predict function - almost finished

--- Algorithm_kNN.py
+++ Algorithm_kNN.py
@@ -8,12 +8,41 @@ class kNN:
         self.learningSet = learningSet
 
     def predict(self, inputArray):
-        for i in self.learningSet:
-            for j in inputArray:
-                kNN.calcEuclideanDistans(j, i)
+
+        for i in inputArray:
+
+            distansAndSpeciesArray = []
+            for j in self.learningSet:
+                distansAndSpeciesArray.append([ kNN.calcEuclideanDistans(i[:4], j[:4]), j[4]])
+                distansAndSpeciesArray.sort()
+
+            speciesArray = []
+            for k in range(self.k):
+                speciesArray.append(distansAndSpeciesArray[k][1])
+
+            answerForExample = kNN.mostFrequentSpecies(speciesArray)
+            print("%s = %s" % (speciesArray, answerForExample))
+
+    def mostFrequentSpecies(speciesArray):
+        species = ""
+        willChangeSpecies = 0
+        for i in speciesArray:
+
+            if(species == "" or willChangeSpecies == 0):
+                species = i
+                willChangeSpecies += 1
+            elif(species == i):
+                willChangeSpecies += 1
+            else:
+                willChangeSpecies -= 1
+
+        return species
+
+
+
 
     def calcEuclideanDistans(vectorX, vectorY):
-        return scipy.spatial.distance.euclidean(vectorX, vectorY)
+        return scipy.spatial.distance.euclidean(numpy.array(vectorX), numpy.array(vectorY))
 
     #todo: def score(self, inputArray, label):
 
@@ -32,16 +61,10 @@ class kNN:
     def setK(self, newK):
         self.k = newK
 
+K = 3
 
-data = kNN.loadDataFromCSVfile("iris.csv")
-testAlgorith = kNN(3, data)
-#print(kNN.getLearningSet(testAlgorith))
-vectorA = [0,0,0,0,0]
-vectorB = [1,1,1,1,1]
-vectorC = [1,2,3,4,5]
-distans = kNN.calcEuclideanDistans(vectorA, vectorB)
-print(distans)
-distans = kNN.calcEuclideanDistans(vectorB, vectorC)
-print(distans)
-distans = kNN.calcEuclideanDistans(vectorA, vectorC)
-print(distans)
+learningData = kNN.loadDataFromCSVfile("iris.data.learning.csv")
+testData = kNN.loadDataFromCSVfile("iris.data.test.csv")
+testAlgorithClass = kNN(K, learningData)
+kNN.predict(testAlgorithClass, testData)
+
