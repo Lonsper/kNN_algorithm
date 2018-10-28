0f06e3666e4298f4324462aad01793b022b97ade
Lonsper <lonsper@gmail.com>
2018-10-29 00:38:26 +0100

    one more test

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ class TestAlgorithmKNN(unittest.TestCase):
 
     def test_predict(self):
         K = 2
-        learningData = kNN.loadDataFromCSVfile("tests/test1_learning_iris.csv")
         testDataNoLabels = kNN.loadDataFromCSVfile("tests/test1_data_iris.csv")
         testAnswerLabels = kNN.loadDataFromCSVfile("tests/test1_answer_iris.csv")
+        learningData = kNN.loadDataFromCSVfile("tests/test1_learning_iris.csv")
 
         testAlgorith = kNN(K, learningData)
 
@@ -22,8 +22,18 @@ class TestAlgorithmKNN(unittest.TestCase):
         self.assertEqual('setosa', answer, 'ups')
 
 
-#    def test_calcEuclideanDistance(self):
+    def test_calcEuclideanDistance(self):
+        self.assertEqual(kNN.calcEuclideanDistance([0, 1], [0, 2]), 1)
+        self.assertEqual(kNN.calcEuclideanDistance([0, -2], [0, 2]), 4)
+        self.assertEqual(kNN.calcEuclideanDistance([0, 0], [0, 0]), 0)
+        self.assertEqual(kNN.calcEuclideanDistance([0, 1, 0], [0, 2, 0]), 1)
 
+    def test_setK(self):
+        learningData = kNN.loadDataFromCSVfile("tests/test1_learning_iris.csv")
+        testAlgorith = kNN(5, learningData)
+
+        testAlgorith.setK(2)
+        self.assertEqual(testAlgorith.getK(), 2)
 
  #   def test_score(self):
 
@@ -34,6 +44,5 @@ class TestAlgorithmKNN(unittest.TestCase):
    # def test_getK(self):
 
 
-#    def test_setK(self):
 if __name__ == '__main__':
     unittest.main()
